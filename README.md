e4c546ac698a55c09b28178b1378d015caa7a593
peter-ehmann <pehmann1@gmail.com>
2025-09-09 17:26:55 -0400

    changed README from md to rmd

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# nfl-picks
-Analysis of NFL lines (+/-) to identify patterns and trends for successful betting strategies.
+NFL Picks
+================
+Peter Ehmann
+2025-09-09
+
+Analysis of NFL lines (+/-) to identify patterns and trends for
+successful betting strategies.
---
+++ data-transfer.R
@@ -0,0 +1,16 @@
+# read Excel dataset
+df <- readxl::read_xlsx("/Users/pje/Documents/Personal/Misc/NFL-picks/NFL-picks.xlsx", sheet = 1)
+
+# filter out games that have not occured yet
+out <- df |>
+  dplyr::filter(Season == 2025 & Week <= 1)
+
+# write as csv format to view on git & download
+write.csv(
+  x    = out, 
+  file = "./data/nfl-picks.csv", row.names = FALSE, na = "")
+
+# write as parquet for analysis
+arrow::write_parquet(
+  x    = out,
+  sink = "./data/nfl-picks.parquet")
